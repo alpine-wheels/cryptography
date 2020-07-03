ce92e27045e578952c8dc5e119f985b3042cc3f3
William Jackson <william@subtlecoolness.com>
2020-07-02 21:28:54 -0500

    Do not use custom builder image

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
-CRYPTOGRAPHY_VERSION="$(/usr/bin/awk -F '==' '{ print $2 }' requirements.txt)"
-/usr/bin/git clone --branch "${CRYPTOGRAPHY_VERSION}" --depth 1 https://github.com/pyca/cryptography.git
+#!/bin/sh
+
+set -e
+
+# install os dependencies
+apk add --no-cache gcc git libffi-dev musl-dev openssl-dev
+
+# find version
+CRYPTOGRAPHY_VERSION="$(awk -F '==' '{ print $2 }' requirements.txt)"
+
+# check out source code
+rm -rf cryptography
+git config --global advice.detachedHead false
+git clone --branch "${CRYPTOGRAPHY_VERSION}" --depth 1 https://github.com/pyca/cryptography.git
+
+# build
 cd cryptography || exit
-/usr/local/bin/python setup.py bdist_wheel
+python setup.py bdist_wheel
