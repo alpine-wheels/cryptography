421b1f2dfe9164fbaf0fa5cb3a5fe969208af46f
William Jackson <william@subtlecoolness.com>
2020-07-03 11:20:00 -0500

    Simplify build script

--- build.sh
+++ build.sh
@@ -3,16 +3,7 @@
 set -e
 
 # install os dependencies
-apk add --no-cache gcc git libffi-dev musl-dev openssl-dev
-
-# find version
-CRYPTOGRAPHY_VERSION="$(awk -F '==' '{ print $2 }' requirements.txt)"
-
-# check out source code
-rm -rf cryptography
-git config --global advice.detachedHead false
-git clone --branch "${CRYPTOGRAPHY_VERSION}" --depth 1 https://github.com/pyca/cryptography.git
+apk add --no-cache gcc libffi-dev musl-dev openssl-dev
 
 # build
-cd cryptography || exit
-python setup.py bdist_wheel
+pip wheel --no-deps --requirement requirements.txt
