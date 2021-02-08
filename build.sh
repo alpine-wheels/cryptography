1a356ddfe259f8d9c323d63def5b6e0b7d73ec41
William Jackson <william@subtlecoolness.com>
2021-02-08 10:45:50 -0600

    Add rust to build dependencies

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-apk add --no-cache gcc libffi-dev musl-dev openssl-dev
+apk add --no-cache gcc libffi-dev musl-dev openssl-dev rust
 
 # build
 pip wheel --no-deps --requirement requirements.txt
